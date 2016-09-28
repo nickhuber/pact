bedb44814f19ae479d8b4ce24cc1e73b45404e6f
Nick Huber <nicholashuber@gmail.com>
2016-09-27 19:17:40 -0700

    fix symlink

--- services/ubuntu/setup-ubuntu.sh
+++ services/ubuntu/setup-ubuntu.sh
@@ -3,7 +3,7 @@ cd /opt/combat_tracker
 apt-get update && apt-get install $(cat ubuntu.depends) -y
 python3 -m venv .venv
 cp services/common/nginx.conf /etc/nginx/conf.d/pact.conf
-ln -snf services/common/letsencrypt-renew.sh /etc/cron.daily/letsencrypt-renew.sh
+ln -snf /opt/combat_tracker/services/common/letsencrypt-renew.sh /etc/cron.daily/letsencrypt-renew.sh
 systemctl restart nginx
 systemctl enable combat_tracker.service
 systemctl start combat_tracker.service
