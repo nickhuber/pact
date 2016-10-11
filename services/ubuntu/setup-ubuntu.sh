0b4dba09e040e709f92369e95d1a5b33e20ea710
Nick Huber <nicholashuber@gmail.com>
2016-10-10 18:17:37 -0700

    fix nginx conf

--- services/ubuntu/setup-ubuntu.sh
+++ services/ubuntu/setup-ubuntu.sh
@@ -5,6 +5,7 @@ python3 -m venv .venv
 cp services/common/nginx.conf /etc/nginx/conf.d/pact.conf
 ln -snf /opt/pact/services/common/letsencrypt-renew.sh /etc/cron.daily/letsencrypt-renew.sh
 useradd pact
+chown -R pact:pact /opt/pact
 systemctl restart nginx
 systemctl enable pact.service
 systemctl start pact.service
