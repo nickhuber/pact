5432d1c3336ebd77b2b812af9232213b3ed4248e
Nick Huber <nicholashuber@gmail.com>
2016-10-10 18:08:07 -0700

    more pact stuff

--- services/ubuntu/setup-ubuntu.sh
+++ services/ubuntu/setup-ubuntu.sh
@@ -4,6 +4,7 @@ apt-get update && apt-get install $(cat ubuntu.depends) -y
 python3 -m venv .venv
 cp services/common/nginx.conf /etc/nginx/conf.d/pact.conf
 ln -snf /opt/pact/services/common/letsencrypt-renew.sh /etc/cron.daily/letsencrypt-renew.sh
+useradd pact
 systemctl restart nginx
 systemctl enable pact.service
 systemctl start pact.service
