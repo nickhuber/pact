8cd0289585bcbde5c1baca181b213699b6795737
Nick Huber <nicholashuber@gmail.com>
2016-12-18 12:25:48 -0800

    tabs to spaces

--- start_frontend.sh
+++ start_frontend.sh
@@ -3,23 +3,23 @@
 set -e
 
 function cleanup() {
-	kill %1
+    kill %1
 }
 
 pushd frontend
-	echo "Updating JavaScript dependencies"
-	npm install
-	npm update
-	./node_modules/.bin/bower install
-	echo "Compiling everything together"
-	npm run grunt
-	npm run grunt -- watch > /dev/null 2> /dev/null &
-	trap cleanup SIGINT SIGTERM
-	npm install http-server
-	echo "Starting development web server"
-	# Don't quit if this fails, http-server is braindead and dies if a proxy request fails
-	set +e
-	while true ; do
-		./node_modules/.bin/http-server -P http://localhost:8000 app -p 8081
-	done
+    echo "Updating JavaScript dependencies"
+    npm install
+    npm update
+    ./node_modules/.bin/bower install
+    echo "Compiling everything together"
+    npm run grunt
+    npm run grunt -- watch > /dev/null 2> /dev/null &
+    trap cleanup SIGINT SIGTERM
+    npm install http-server
+    echo "Starting development web server"
+    # Don't quit if this fails, http-server is braindead and dies if a proxy request fails
+    set +e
+    while true ; do
+        ./node_modules/.bin/http-server -P http://localhost:8000 app -p 8081
+    done
 popd
