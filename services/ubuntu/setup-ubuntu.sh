5f87557ea254468bacc14544949d7415f296ef6b
Nick Huber <nicholashuber@gmail.com>
2016-12-19 11:15:25 -0800

    some more cross platform fixes

--- services/ubuntu/setup-ubuntu.sh
+++ services/ubuntu/setup-ubuntu.sh
@@ -3,7 +3,7 @@ cd /opt/pact
 apt-get update && apt-get install $(cat ubuntu.depends) -y
 python3 -m venv .venv
 cp services/common/nginx.conf /etc/nginx/conf.d/pact.conf
-ln -snf /opt/pact/services/common/letsencrypt-renew.sh /etc/cron.daily/letsencrypt-renew.sh
+ln -snf /opt/pact/services/common/letsencrypt-renew /etc/cron.daily/letsencrypt-renew.sh
 useradd pact
 chown -R pact:pact /opt/pact
 systemctl restart nginx
