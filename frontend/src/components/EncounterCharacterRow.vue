5169d1439cb9b0d20677414e793050e11fd0e2e8
Nick Huber <nicholashuber@gmail.com>
2019-04-22 10:19:40 -0700

    make the encounter table look a bit better

--- frontend/src/components/EncounterCharacterRow.vue
+++ frontend/src/components/EncounterCharacterRow.vue
@@ -1,32 +1,59 @@
 <template>
 <tr>
     <td>
-        <span v-if="!character.is_player">
-            <span @click="showDescription">
-                <a href="javascript://">{{ character.name }}</a>
+        <div class="vertical-center">
+            <span v-if="!character.is_player">
+                <span @click="showDescription">
+                    <a href="javascript://">{{ character.name }}</a>
+                </span>
             </span>
-        </span>
-        <span v-if="character.is_player">
-            {{ character.name }}
-        </span>
+            <span v-if="character.is_player">
+                {{ character.name }}
+            </span>
+        </div>
     </td>
     <td>
-    </td>
-    <td>
-        <div class="level">
-            <div class="level-item">
-                {{ character.initiative }}
+        <div class="vertical-center">
+            <div v-if="!notesEditing">
+                <div class="level">
+                    <div class="level-item">
+                        <span>{{ character.notes }}</span>
+                    </div>
+                    <div class="level-item">
+                        <button class="button is-small" @click="enableEditing">Edit</button>
+                    </div>
+                </div>
             </div>
-            <div class="level-item">
-                <button class="button is-small" @click="increaseInitiative" :disabled="formsDisabled"><font-awesome-icon icon="arrow-up"></font-awesome-icon></button>
-                <button class="button is-small" @click="decreaseInitiative" :disabled="formsDisabled"><font-awesome-icon icon="arrow-down"></font-awesome-icon></button>
+            <div v-if="notesEditing" class="level">
+                <div class="level-item">
+                    <input v-model="newNotes" class="input is-small"/>
+                </div>
+                <div class="level-item">
+                    <button class="button is-small" @click="disableEditing">Cancel</button>
+                </div>
+                <div class="level-item">
+                    <button class="button is-small" @click="saveEdit">Save</button>
+                </div>
             </div>
         </div>
     </td>
     <td>
-        <span v-if="!character.is_player">
+        <div class="vertical-center">
             <div class="level">
                 <div class="level-item">
+                    {{ character.initiative }}
+                </div>
+                <div class="level-item">
+                    <button class="button is-small" @click="increaseInitiative" :disabled="formsDisabled"><font-awesome-icon icon="arrow-up"></font-awesome-icon></button>
+                    <button class="button is-small" @click="decreaseInitiative" :disabled="formsDisabled"><font-awesome-icon icon="arrow-down"></font-awesome-icon></button>
+                </div>
+            </div>
+        </div>
+    </td>
+    <td>
+        <div v-if="!character.is_player" class="vertical-center">
+            <div class="columns">
+                <div class="column is-one-fifth">
                     <div class="fraction">
                         <span class="fraction-numerator">
                             {{ character.current_hp }}
@@ -39,33 +66,42 @@
                         </span>
                     </div>
                 </div>
-                <div class="level-item">
-                    <button class="button is-fullwidth is-warning" @click="hurt" :disabled="formsDisabled">Hurt</button>
+                <div class="column">
+                    <div class="level">
+                        <div class="level-item">
+                            <button class="button is-small is-fullwidth is-warning" @click="hurt" :disabled="formsDisabled">Hurt</button>
+                        </div>
+                        <div class="level-item is-two-fifths">
+                            <input class="input is-small" type="number" v-model.number="hp_change_value" :disabled="formsDisabled">
+                        </div>
+                        <div class="level-item">
+                            <button class="button is-small is-fullwidth is-success" @click="heal" :disabled="formsDisabled">Heal</button>
+                        </div>
+                    </div>
                 </div>
-                <div class="level-item is-two-fifths">
-                    <input class="input" type="number" v-model.number="hp_change_value" :disabled="formsDisabled">
-                </div>
-                <div class="level-item">
-                    <button class="button is-fullwidth is-success" @click="heal" :disabled="formsDisabled">Heal</button>
-                </div>
-            </div>
-        </span>
-    </td>
-    <td>
-        <div class="list" v-if="character.status_effects.length > 0">
-            <div class="list-item" v-for="status in character.status_effects" :key="status.url">
-                <span>
-                    <a @click="changeStatusEffect(status)">{{ status.name }}</a> - {{ status.remaining_duration }} turn<span v-if="status.remaining_duration > 1">s</span>
-                </span>
-                <a class="is-pulled-right" @click="removeStatusEffect(status)">
-                    <font-awesome-icon icon="times"></font-awesome-icon>
-                </a>
             </div>
         </div>
     </td>
     <td>
-        <button class="button is-danger" @click="remove" :disabled="formsDisabled">Remove</button>
-        <button class="button is-info" @click="addStatusEffect" :disabled="formsDisabled">Add status effect</button>
+        <div v-if="character.status_effects.length > 0" class="vertical-center">
+            <ul class="status-list">
+                <li v-for="status in character.status_effects" :key="status.url">
+                    <span>
+                        <a @click="changeStatusEffect(status)">{{ status.name }}</a> - {{ status.remaining_duration }} turn<span v-if="status.remaining_duration > 1">s</span>
+                    </span>
+                    <a class="is-pulled-right" @click="removeStatusEffect(status)">
+                        <font-awesome-icon icon="times"></font-awesome-icon>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </td>
+    <td>
+        <div class="vertical-center">
+            <button class="is-vcentered button is-small is-danger" @click="remove" :disabled="formsDisabled">Remove</button>
+            <button class="is-vcentered button is-small is-info" @click="addStatusEffect" :disabled="formsDisabled">Add status effect</button>
+        </div>
     </td>
 </tr>
 </template>
@@ -81,28 +117,36 @@ export default {
     data() {
         return {
             hp_change_value: null,
+            notesEditing: false,
+            newNotes: '',
         }
     },
     methods: {
         hurt() {
+            if (this.hp_change_value == null || this.hp_change_value == '') {
+                return;
+            }
             this.$http.patch(
                 `/api/encounter_characters/${this.character.uuid}`,
                 {
                     current_hp: this.character.current_hp - this.hp_change_value
                 }
-            ).then((response) => { 
+            ).then((response) => {
                 this.character.current_hp = response.data.current_hp;
                 this.$emit('encounter-character-updated');
                 this.hp_change_value = null;
             });
         },
         heal() {
+            if (this.hp_change_value == null || this.hp_change_value == '') {
+                return;
+            }
             this.$http.patch(
                 `/api/encounter_characters/${this.character.uuid}`,
                 {
                     current_hp: this.character.current_hp + this.hp_change_value
                 }
-            ).then((response) => { 
+            ).then((response) => {
                 this.character.current_hp = response.data.current_hp;
                 this.$emit('encounter-character-updated');
                 this.hp_change_value = null;
@@ -114,7 +158,7 @@ export default {
                 {
                     initiative: this.character.initiative + 1
                 }
-            ).then((response) => { 
+            ).then((response) => {
                 this.character.initiative = response.data.initiative;
                 this.$emit('encounter-character-updated');
             });
@@ -125,7 +169,7 @@ export default {
                 {
                     initiative: this.character.initiative - 1
                 }
-            ).then((response) => { 
+            ).then((response) => {
                 this.character.initiative = response.data.initiative;
                 this.$emit('encounter-character-updated');
             });
@@ -133,7 +177,7 @@ export default {
         remove() {
             this.$http.delete(
                 `/api/encounter_characters/${this.character.uuid}`
-            ).then((response) => { 
+            ).then((response) => {
                 this.$emit('encounter-character-updated');
             });
         },
@@ -174,11 +218,35 @@ export default {
                 this.$emit('encounter-character-updated');
             });
         },
+        enableEditing: function(){
+            this.newNotes = this.character.notes;
+            this.notesEditing = true;
+        },
+        disableEditing: function(){
+            this.newNotes = null;
+            this.notesEditing = false;
+        },
+        saveEdit: function(){
+            this.$http.patch(
+                `/api/encounter_characters/${this.character.uuid}`,
+                {
+                    notes: this.newNotes
+                }
+            ).then((response) => {
+                this.character.notes = response.data.notes;
+                this.$emit('encounter-character-updated');
+                this.hp_change_value = null;
+            });
+            this.disableEditing();
+        }
     }
 }
 </script>
 
 <style scoped lang="scss">
+.columns {
+    margin-bottom: 0px;
+}
 .fraction {
     display: inline-block;
     position: relative;
@@ -190,10 +258,33 @@ export default {
         display: block;
     }
     span.fraction-denominator {
-            border-top: thin solid  ;
+        border-top: thin solid;
     }
     span.fraction-bar {
-            display: none;
+        display: none;
     }
+}
+
+.status-list {
+    margin: 0px;
+    list-style: none;
+    width: 100%;
+}
+
+// Vertical center the things in the table cells
+td div {
+    height: 100%;
+    margin: 0;
+    padding:0;
+}
+tr, td {
+    height: 100%;
+}
+
+.vertical-center {
+    display: flex;
+    align-items: center;
+    height: 100%;
+
 }
 </style>
