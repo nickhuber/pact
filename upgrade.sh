208aa0a28b29e047be89519f698ef8a92ac12a76
Nick Huber <nicholashuber@gmail.com>
2019-04-21 22:31:22 -0700

    fix upgrade script to work with migrations

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /opt/pact/.venv/bin/pip install -r /opt/pact/backend/requirements.txt --upgrade
-/opt/pact/.venv/bin/python /opt/pact/backend/pact/manage.py migrate
+sudo -u pact /opt/pact/.venv/bin/python /opt/pact/backend/pact/manage.py migrate
 /opt/pact/.venv/bin/python /opt/pact/backend/pact/manage.py collectstatic --clear --noinput
 
 pushd /opt/pact/frontend/
